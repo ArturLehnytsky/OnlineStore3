1729758c39dcecec5d65f98e25e21b34bda095bc
Artur Lekhnitskyi <arturlehnytsky@gmail.com>
2019-02-04 23:24:04 +0200

    commit 1

---
+++ OnlineStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using OnlineStore.Domain.Abstract;
+using OnlineStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+
+    {
+        private readonly EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}
