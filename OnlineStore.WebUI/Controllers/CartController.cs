deb180fe370ddbae2721d2455afdd35f3385c466
Artur Lekhnitskyi <arturlehnytsky@gmail.com>
2019-02-05 23:37:21 +0200

    Built Shopping Cart

---
+++ OnlineStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,60 @@
+﻿using OnlineStore.Domain.Abstract;
+using OnlineStore.Domain.Entities;
+using OnlineStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(
+                new CartIndexViewModel { Cart = GetCart(), ReturnUrl = returnUrl });
+        }
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductId == productId);
+
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductId == productId);
+
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+}
